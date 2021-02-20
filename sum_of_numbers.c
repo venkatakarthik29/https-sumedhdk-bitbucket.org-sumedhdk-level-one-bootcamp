01fd7420c10ab4c4312ce7af5a899d8a543941c3
munnangi venkatakarthik <75118794+venkatakarthik29@users.noreply.github.com>
2021-02-20 13:16:49 +0530

    Update sum_of_numbers.c

--- sum_of_numbers.c
+++ sum_of_numbers.c
@@ -1,1 +1,54 @@
 //Write a program to find the sum of n different numbers using 4 functions
+#include<stdio.h>
+
+int IntputLimit();
+int* ReadArray();
+int CalculateSum();
+void DisplaySum();
+
+
+int main()
+{
+    int n,sum=0;
+    n=IntputLimit();
+    int arr[n];
+    int* ptr=ReadArray(n,arr);
+    sum=CalculateSum(n,ptr);
+    DisplaySum(sum);   
+}
+
+
+int IntputLimit()
+{
+    int n;
+    printf("Enter the input limit : ");
+    scanf("%d",&n);
+    return n;
+}
+
+int* ReadArray(int n,int arr[])
+{
+    int i;
+    for(i=0;i<n;i++)
+    {
+        printf("Enter the %d element : ",i);
+        scanf("%d",&arr[i]);
+        
+    }
+    return arr;
+}
+
+int CalculateSum(int n,int *ptr)
+{
+    int i,sum=0;
+    for(i=0;i<n;i++)
+    {
+        sum = sum + *(ptr+i);
+    }
+    return sum;
+}
+
+void DisplaySum(int sum)
+{
+    printf("sum of the given numbers is : %d",sum);
+}
